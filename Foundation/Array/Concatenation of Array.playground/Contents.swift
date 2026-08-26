cae23961d443828cdd0a4bed44ff93dd687e4243
SynxGentox <varyan069@gmail.com>
2026-08-27 01:47:56 +0530

    Update Contents.swift

--- Foundation/Array/Concatenation of Array.playground/Contents.swift
+++ Foundation/Array/Concatenation of Array.playground/Contents.swift
@@ -1,7 +1,31 @@
 import UIKit
 
-class Concatenation {
-    func getConcatenated(nums: [Int]) -> [Int] {
-        return Array(repeating: nums, count: 2).flatMap { $0 }
+// Standard Concatenation i.e. to concatenate only 2 times
+class StandardConcatenation {
+    func getConcatenation(nums: [Int]) -> [Int] {
+        var arr = nums
+        arr += arr
+        /* arr.append(contentsOf: arr) */
+        return arr
+    }
+}
+
+// If Problem asks to Concatenate T times
+class TConcatenation {
+    func getConcatenated(nums: [Int], times: Int) -> [Int] {
+        return Array(repeating: nums, count: times).flatMap { $0 }
+    }
+}
+
+class TConcatenationOtherWay {
+    func getConcatenation(nums: [Int], times: Int) -> [Int] {
+        var ans: [Int] = []
+        
+        ans.reserveCapacity(nums.count * times)
+        
+        for _ in 0..<times {
+            ans.append(contentsOf: nums)
+        }
+        return ans
     }
 }
