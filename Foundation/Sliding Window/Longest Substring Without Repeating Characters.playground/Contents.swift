2deb4a1221289bc03601a3130abec27e96549bc8
SynxGentox <varyan069@gmail.com>
2026-09-14 23:56:14 +0530

    .

--- Foundation/Sliding Window/Longest Substring Without Repeating Characters.playground/Contents.swift
+++ Foundation/Sliding Window/Longest Substring Without Repeating Characters.playground/Contents.swift
@@ -1,26 +1,71 @@
+// MARK: - HashMap Solution (General and Fairly Fast)
+// Time Complexity: O(n), Space Complexity: O(1)
+/// Space Complexity:  This solution is O(1) in space complexity as it doesnt scales with output increase, for input that are in english the HashMap can only extend to 26 letters for each Case.
+/// Time Complexity: This is a True O(n) time scaling solution, however it has heavy hashing and probing Overhead.
+
+// Working:
+/// This solution uses HashMap to keep track of dublicates and to iterate of String we are using String.Index bcz direct iteration over string has O(n) time scaling Look-up , while we can convert String to Array but would mean O(n) space.
+/// String.Index to iterate over String, left and right pointer to keep track of length
+
+
 class Solution {
     func lengthOfLongestSubstring(_ s: String) -> Int {
+        // Early exit for string with length < 1.
         guard s.count > 1 else { return s.count }
+        // HashMap to keep track of dublicates.
+        var lastSeen: [Character: Int] = [:]
+        lastSeen.reserveCapacity(s.count)
         
-        var sTracker: [Character: String.Index] = [:]
-        sTracker.reserveCapacity(s.count)
-        
-        var left = s.startIndex
-        var right = left
+        // left and right pointer to keep the track of the length using a sliding window.
+        var leftPos = 0
+        var rightPos = 0
+        // right - String.Index to iterate over String in O(1) time and O(1) space complexity.
+        var rightIdx = s.startIndex
         var maxLen = 0
-        var count = 0
         
-        while right < s.endIndex {
-            let char = s[right]
-            if let seen = sTracker[char], seen >= left {
-                left = s.index(after: seen)
-                count = 0
+        while rightIdx < s.endIndex {
+            let char = s[rightIdx]
+            
+            // If we locate a dublicates we update Left pointer to next element of the last element seen.
+            if let seenPos = lastSeen[char], seenPos >= leftPos {
+                leftPos = seenPos + 1
             }
-            sTracker[char] = right
-            maxLen = max(maxLen, (s.distance(from: left, to: right) + 1))
-            right = s.index(after: right)
-            count += 1
+            // updating the saved previous String.Index of character 'char' to new right pointer which is holding the index values
+            lastSeen[char] = rightPos
+            // updaing the maxLen, length of longest non-repeating subString.
+            maxLen = max(maxLen, rightPos - leftPos + 1)
+            // updating the right String.Index to next String.Index of it.
+            rightIdx = s.index(after: rightIdx)
+            // incrementing the right pointer by 1
+            rightPos += 1
         }
         return maxLen
+    }
+}
+
+
+// MARK: - Array Solution (Fixed Sized Array, Constraints biased)
+// Time Complexity: O(n), Space Complexity: O(n)
+class Solution2 {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        let chars = Array(s.utf8)
+
+        var lastSeen = Array(repeating: -1, count: 128)
+        var left = 0
+        var maxLength = 0
+
+        for right in 0..<chars.count {
+            let char = Int(chars[right])
+
+            if lastSeen[char] >= left {
+                left = lastSeen[char] + 1
+            }
+
+            lastSeen[char] = right
+
+            maxLength = max(maxLength, right - left + 1)
+        }
+
+        return maxLength
     }
 }
