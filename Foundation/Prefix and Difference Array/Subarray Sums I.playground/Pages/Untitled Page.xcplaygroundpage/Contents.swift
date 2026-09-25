782e84f9804c44ee55fd4cc059abf5bb2fd01b0e
SynxGentox <varyan069@gmail.com>
2026-09-25 17:31:07 +0530

    solved

--- Foundation/Prefix and Difference Array/Subarray Sums I.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Foundation/Prefix and Difference Array/Subarray Sums I.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,3 +1,4 @@
+// MARK: - Sliding Window Solution
 class Solution {
     func main(_ n: Int, _ x: Int, _ arr: [Int]) {
         var left = 0
@@ -5,7 +6,7 @@ class Solution {
         var sum = 0
         var count = 0
         
-        while right < n, left < n {
+        while left < n {
             if sum > x {
                 sum -= arr[left]
             }
@@ -14,8 +15,45 @@ class Solution {
                 sum -= arr[left]
                 left += 1
             }
-            sum += arr[right]
-            right += 1
+            if right < n {
+                sum += arr[right]
+                right += 1
+            }
+            else if right == n {
+                left += 1
+            }
+        }
+        
+        print("final count: \(count)")
+    }
+}
+
+Solution().main(5, 7, [2, 4, 1, 2, 7])
+
+// MARK: - Prefix Solution
+class Solution2 {
+    func main(_ n: Int, _ x: Int, _ arr: [Int]) {
+        var left = 0
+        var right = 0
+        var sum = 0
+        var count = 0
+        
+        while left < n {
+            if sum > x {
+                sum -= arr[left]
+            }
+            else if sum == x {
+                count += 1
+                sum -= arr[left]
+                left += 1
+            }
+            if right < n {
+                sum += arr[right]
+                right += 1
+            }
+            else if right == n {
+                left += 1
+            }
         }
         print("final count: \(count)")
     }
